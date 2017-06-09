1909b52e212bc1d8e90cfbe1579bc4b1a956a3e3
Tommi <tommi.tiira@gmail.com>
2017-06-10 01:01:35 +0300

    first try on response based on Google example

--- command/listen.go
+++ command/listen.go
@@ -1,4 +1,41 @@
-package main
+package listener
 
-func main() {
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+)
+
+type slashResponse struct {
+	ResponseType string `json:"response_type"`
+	Text         string `json:"text"`
+}
+
+func init() {
+	http.HandleFunc("/", handleMessage)
+}
+
+func handleMessage(w http.ResponseWriter, r *http.Request){
+
+	if token != "" && r.PostFormValue("token") != token {
+		http.Error(w, "Invalid Slack token.", http.StatusBadRequest)
+		return
+	}
+	c := appengine.NewContext(r)
+	log.Errorf(c, "Got token: %s", r.PostFormValue("token"))
+	
+	w.Header().Set("content-type", "application/json")
+
+	resp := &slashResponse{
+		ResponseType: "in_channel",
+		Text:         answers[rand.Intn(len(answers))],
+	}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Errorf(c, "Error encoding JSON: %s", err)
+		http.Error(w, "Error encoding JSON.", http.StatusInternalServerError)
+		return
+	}
 }
