7f01b61a9a581d3bd8dfe0d413efe3faaae311c0
Tommi <tommi.tiira@gmail.com>
2017-06-11 22:16:18 +0300

    testing on the responses

--- command/listen.go
+++ command/listen.go
@@ -7,33 +7,53 @@ import (
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
+	"strings"
+	"net/url"
 )
+
+type attachments struct {
+	Text         string `json:"text"`
+}
 
 type slashResponse struct {
 	ResponseType string `json:"response_type"`
 	Text         string `json:"text"`
-	Attachments  []string	`json:"attachments"`
+	Attachments  []*attachments `json:"attachments"`
 }
 
 func init() {
 	http.HandleFunc("/", handleMessage)
 }
 
-func handleMessage(w http.ResponseWriter, r *http.Request){
+func handleMessage(w http.ResponseWriter, r *http.Request) {
+
+	var token= tokentest
+
+	if !appengine.IsDevAppServer() {
+		token = tokenprod
+	}
 
 	if token != "" && r.PostFormValue("token") != token {
 		http.Error(w, "Invalid Slack token.", http.StatusBadRequest)
 		return
 	}
+
 	c := appengine.NewContext(r)
 	log.Errorf(c, "Got token: %s", r.PostFormValue("token"))
 
 	w.Header().Set("content-type", "application/json")
 
+	att := &attachments{
+		Text: r.PostFormValue("text"),
+	}
+
+	var attJson = att
+
 	resp := &slashResponse{
 		ResponseType: "in_channel",
-		Text:          "Kiitos "+r.PostFormValue("user_name")+ "! " + answers[rand.Intn(len(answers))],
-		Attachments:   []string{"{"+r.PostFormValue("text")+"}"},
+		Text:         "Kiitos " + r.PostFormValue("user_name") + "! " + answers[rand.Intn(len(answers))],
+		Attachments:  []*attachments{attJson, },
 	}
 
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
@@ -41,4 +61,24 @@ func handleMessage(w http.ResponseWriter, r *http.Request){
 		http.Error(w, "Error encoding JSON.", http.StatusInternalServerError)
 		return
 	}
+
+	print(json.NewEncoder(w).Encode(resp))
+
+	//sendForm
+
+}
+
+//How is sending a POST-request from AppEngine supposed to work...
+func sendForm() {
+
+	payload := url.Values{qtarget: {"asdf"}, qtext: {"asdf asdf"}, qreporter: {"asdf sadf"}}
+
+	r,_ := http.NewRequest("POST",formurl,strings.NewReader(payload.Encode()))
+
+
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+
+	client.Do(r)
+
 }
