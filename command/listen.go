5b39894417fcd50410fe31a031b22e274049e533
Tommi <tommi.tiira@gmail.com>
2017-06-21 22:42:45 +0300

    Reporting functionality, refactoring, commenting and minor bug fixes

--- command/listen.go
+++ command/listen.go
@@ -1,3 +1,7 @@
+//Package for listening slack slashcommands, checking the data for
+//keywords (targets) listed in google sheet, saving the data to
+//another google sheet and responding to the request. Also provides
+//instructions of use and reporting of saved data per target or all
 package listener
 
 import (
@@ -25,6 +29,8 @@ type slashResponse struct {
 	Attachments  []*attachments `json:"attachments"`
 }
 
+//Info for team is given in a separate .go file and saved to
+//a map in configuration .go file
 type teamInfo struct {
 	PrivateSlackurl     string
 	PublicSlackurl      string `text:"omitempty"`
@@ -41,78 +47,105 @@ type teamInfo struct {
 
 var team teamInfo
 
+//Direct the request
 func init() {
 	http.HandleFunc("/", handleMessage)
 }
 
+//the main function for POST-request handling
 func handleMessage(w http.ResponseWriter, r *http.Request) {
 
 	team = getTeamInfo(r.PostFormValue("token"))
 
+	//if team info is not found, the token is invalid
+	//team info is in a map-object in the configuration file
+	//with token as key
 	if len(team.PublicSlackurl) == 0 {
 		http.Error(w, "Invalid Slack token.", http.StatusBadRequest)
 		return
 	}
 
-	c := appengine.NewContext(r)
-
+	ctx := appengine.NewContext(r)
 	w.Header().Set("content-type", "application/json")
 
-	sender := r.PostFormValue("user_name")
+	//escape problematic characters
 	message := strings.Replace(strings.Replace(r.PostFormValue("text"), `"`, "´´", -1), "\\", "/", -1)
 
-	att := &attachments{
-		Text: message,
+	//If the request is a valid report, do the following steps,
+	//else return appropriate error-message
+	resp, isValid := createResponse(r, message)
+	if isValid {
+		sendSlackMsg(message, r, true)
+		saveDataToSheets(r, message)
 	}
 
-	var attJSON = att
-	var resp = &slashResponse{}
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		log.Errorf(ctx, "Error encoding JSON: %s", err)
+	}
+}
 
-	saveMessageResp := saveDataToSheets(r, sender, message)
+//Creates the response for the initial POST-request. The response
+//includes an ephemeral slack-message
+func createResponse(r *http.Request, message string) (*slashResponse, bool) {
 
+	//If the message is -help, return the helptext gicen in the team info
 	if strings.EqualFold(strings.ToLower(message), "-help") {
-		resp = &slashResponse{
+		return &slashResponse{
 			ResponseType: "ephemeral",
 			Text:         team.HelpText,
-		}
-	} else if strings.HasPrefix(strings.ToLower(message), "-hae") {
-		resp = &slashResponse{
+		}, false
+	}
+
+	//If the message is -hae, return a list of reports for given target or
+	//all targets if target is "kaikki"
+	if strings.HasPrefix(strings.ToLower(message), "-hae") {
+		return &slashResponse{
 			ResponseType: "ephemeral",
 			Text:         getTargetReports(r, message),
-		}
-	} else if saveMessageResp == "" {
-		resp = &slashResponse{
-			ResponseType: "ephemeral",
-			Text:         "Kiitos " + sender + "! " + team.Answer,
-			Attachments:  []*attachments{attJSON},
-		}
+		}, false
+	}
 
-		sendSlackMsg(message, r, true)
+	//Find the target in message
+	target := findTarget(r, message)
 
-	} else if saveMessageResp == "noTarget" {
-		resp = &slashResponse{
+	//If target is not found, respond with team specific message
+	if target == "noTarget" {
+		return &slashResponse{
 			ResponseType: "ephemeral",
 			Text:         team.NoTargetMessage,
-			Attachments:  []*attachments{attJSON},
-		}
-	} else {
-		resp = &slashResponse{
+			Attachments: []*attachments{&attachments{
+				Text: message,
+			}},
+		}, false
+	}
+
+	//Target found in message, the request is a valid report
+	if len(target) > 0 {
+
+		return &slashResponse{
 			ResponseType: "ephemeral",
-			Text:         team.ErrorMessage,
-			Attachments:  []*attachments{attJSON},
-		}
+			Text:         "Kiitos " + r.PostFormValue("user_name") + "! " + team.Answer,
+			Attachments: []*attachments{&attachments{
+				Text: message,
+			}},
+		}, true
+
 	}
 
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		log.Errorf(c, "Error encoding JSON: %s", err)
-	}
-
+	return &slashResponse{
+		ResponseType: "ephemeral",
+		Text:         team.ErrorMessage,
+		Attachments: []*attachments{&attachments{
+			Text: message,
+		}},
+	}, false
 }
 
 // Send Slack message to dedicated channel as bot user
 func sendSlackMsg(message string, r *http.Request, public bool) {
 
-	if !strings.Contains(message, team.NoPostKey) {
+	if strings.Contains(message, team.NoPostKey) {
 		return
 	}
 
@@ -121,6 +154,7 @@ func sendSlackMsg(message string, r *http.Request, public bool) {
 	ctx := appengine.NewContext(r)
 	client := urlfetch.Client(ctx)
 
+	//for separate channels depending on keywords in message, not in use currently
 	/*if public {
 		req, _ := http.NewRequest("POST", team.PublicSlackurl, payload)
 	} else {
@@ -130,17 +164,16 @@ func sendSlackMsg(message string, r *http.Request, public bool) {
 	req, _ := http.NewRequest("POST", team.PublicSlackurl, payload)
 	req.Header.Set("Content-Type", "application/json")
 
-	resp2, err := client.Do(req)
+	_, err := client.Do(req)
 
 	if err != nil {
 		log.Errorf(ctx, "Unable to send message as bot user: %s", err)
 	}
 
-	defer resp2.Body.Close()
-
 }
 
-func saveDataToSheets(r *http.Request, sender string, message string) string {
+//Writes timestamp, target name, message and sender name to team-specific Google sheet
+func saveDataToSheets(r *http.Request, message string) string {
 
 	ctx := appengine.NewContext(r)
 	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/spreadsheets")
@@ -155,16 +188,7 @@ func saveDataToSheets(r *http.Request, sender string, message string) string {
 		return "Error"
 	}
 
-	targets, err := srv.Spreadsheets.Values.Get(team.TargetSpreadsheetID, team.ReadRange).Context(ctx).Do()
-	if err != nil {
-		log.Errorf(ctx, "Unable to retrieve data from targetsheet. %v", err)
-		return "Error"
-	}
-
-	var target = parseKeywords(message, targets)
-	if target == "" {
-		return "noTarget"
-	}
+	target := findTarget(r, message)
 
 	layout := "01/02/2006 15:04:05"
 	timestamp := time.Now().Format(layout)
@@ -172,8 +196,8 @@ func saveDataToSheets(r *http.Request, sender string, message string) string {
 	valueInputOption := "RAW"
 	var vr sheets.ValueRange
 
-	myval := []interface{}{timestamp, target, message, sender}
-	vr.Values = append(vr.Values, myval)
+	saveData := []interface{}{timestamp, target, message, r.PostFormValue("user_name")}
+	vr.Values = append(vr.Values, saveData)
 
 	_, err = srv.Spreadsheets.Values.Append(team.ReportSpreadsheetID, team.WriteRange, &vr).ValueInputOption(valueInputOption).Context(ctx).Do()
 	if err != nil {
@@ -185,14 +209,36 @@ func saveDataToSheets(r *http.Request, sender string, message string) string {
 
 }
 
-func parseKeywords(message string, keywords *sheets.ValueRange) string {
+//Looks for targets in message. Targets are listed in team-specific Google sheet
+//returns the target's name or "noTarget" if target wasn't found and empty string
+//if there's an error reading the target list
+func findTarget(r *http.Request, message string) string {
 
-	if len(keywords.Values) > 0 {
+	ctx := appengine.NewContext(r)
+	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Errorf(ctx, "Unable to create client %s", err)
+		return ""
+	}
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Errorf(ctx, "Unable to retrieve Sheets Client %v", err)
+		return ""
+	}
+
+	targets, err := srv.Spreadsheets.Values.Get(team.TargetSpreadsheetID, team.ReadRange).Context(ctx).Do()
+	if err != nil {
+		log.Errorf(ctx, "Unable to retrieve data from targetsheet. %v", err)
+		return ""
+	}
+
+	if len(targets.Values) > 0 {
 
 		fullname := ""
 		shortname := ""
 
-		for _, row := range keywords.Values {
+		for _, row := range targets.Values {
 
 			fullname = row[0].(string)
 			if len(row) > 1 {
@@ -218,9 +264,12 @@ func parseKeywords(message string, keywords *sheets.ValueRange) string {
 		}
 	}
 
-	return ""
+	return "noTarget"
+
 }
 
+//finds reports on the given target or all reports if target is "kaikki"
+//return all reports formatted into one string
 func getTargetReports(r *http.Request, message string) string {
 
 	ctx := appengine.NewContext(r)
@@ -242,14 +291,25 @@ func getTargetReports(r *http.Request, message string) string {
 		return "Error"
 	}
 
-	target := strings.ToLower(message[strings.Index(message, " "):len(message)])
+	target := ""
+
+	if strings.EqualFold(message[strings.Index(message, " "):len(message)], "kaikki") {
+		target = "kaikki"
+	} else {
+		target = findTarget(r, message)
+	}
+
+	if strings.EqualFold(target, "noTarget") {
+		return team.NoTargetMessage
+	}
+
 	reports := []string{""}
 
 	if len(data.Values) > 0 {
 
 		for _, row := range data.Values {
 
-			if strings.EqualFold(strings.ToLower(row[0].(string)), target) {
+			if strings.EqualFold(target, "kaikki") || strings.EqualFold(strings.ToLower(row[0].(string)), target) {
 			}
 			reports = append(reports, row[1].(string)+" Reporter: "+row[2].(string))
 		}
@@ -259,7 +319,7 @@ func getTargetReports(r *http.Request, message string) string {
 		return "No reports found"
 	}
 
-	targetReports := "Reports for target" + target + "\n"
+	targetReports := "Reports for target " + target + "\n"
 
 	for _, report := range reports {
 
